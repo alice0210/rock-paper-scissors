b512215e9b7eba76b506aae85ac689e2f8c1b416
Alice <alicepygott96@gmail.com>
2022-11-02 15:33:49 +0000

    Add scores function

--- script.js
+++ script.js
@@ -11,41 +11,69 @@ function getComputerChoice() {
     }      
 }
 
+let playerScore=0;
+let computerScore=0;
+
 
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
+        computerScore += 0;
+        playerScore += 0;
         return "It's a tie!";
+        
     }
 
     if (playerSelection === "rock") {
 
         if (computerSelection === "scissors") {
+            playerScore += 1;
             return "You Win! Rock beats Scissors.";
+            
         } else {
+            computerScore += 1;
             return "You Lose! Paper beats Rock.";
+            
         }
     }
 
     else if (playerSelection === "paper") {
 
         if (computerSelection === "rock") {
+            playerScore += 1;
             return "You Win! Paper beats Rock.";
+            
         } else {
+            computerScore += 1;
             return "You Lose! Scissors beats Paper.";
+            
         }
     }
 
     else if (playerSelection === "scissors") {
 
         if (computerSelection === "paper") {
+            playerScore += 1;
             return "You Win! Scissors beats Paper.";
+            
         } else {
+            computerScore += 1;
             return "You Lose! Rock beats Scissors.";
+            
         }
     }
 }
 
+function scores () {
+    
+    if (playerScore > computerScore) {
+        return "You win!";
+    } else if (computerScore > playerScore) {
+        return "You lose!";
+    } else {
+        return "It's a tie!";
+    }
+}
 
 function game() {
 
@@ -55,12 +83,19 @@ function game() {
 
         let computerSelection = getComputerChoice();
         
-        console.log("Player Selection: " +playerSelection);
+        console.log("Your Selection: " +playerSelection);
 
         console.log("Computer Selection: " +computerSelection);
 
         console.log(playRound(playerSelection, computerSelection));
+
+        console.log("Your score: " +playerScore);
+
+        console.log("Computer score: " +computerScore);
+
     }
 }
 
-console.log(game());
+
+game();
+console.log(scores());
